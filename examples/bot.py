5f89c64fe09e97f3b46dfd3271bf3ec4503e5c36
Oleg Petrakov <murami.ike@gmail.com>
2025-11-10 00:26:39 +0400

    Fix handlers process

--- examples/bot.py
+++ examples/bot.py
@@ -1,26 +1,52 @@
 from ikemurami.telegram import telegram_bot
 import logging
 
-from telegram import Update
+from functools import wraps
+
+from telegram import Update, BotCommand
 from telegram.ext import (
     ApplicationHandlerStop,
     TypeHandler,
     MessageHandler,
+    CommandHandler,
     filters
 )
+
+
+ADMINS = [
+    394898882,
+]
+USERS = set()
 
 
 logger = logging.getLogger('tgbot')
 
 
-async def is_admin(update, context):
-    ADMINS = []
-    user_id = update.message.from_user.id
-    logger.info(f'Checking admin permissions for telegram user id: {user_id}')
-    if user_id not in ADMINS:
-        raise ApplicationHandlerStop
+def admin_check(func):
+    @wraps(func)
+    async def wrapper(update: Update, context):
+        user_id = update.effective_user.id if update.effective_user else None
+        if user_id not in ADMINS:
+            logger.warning(f'Unauthorized access denied for user {user_id}.')
+            return
+        return await func(update, context)
+    return wrapper
 
 
+@admin_check
+async def hello_handler(update, context):
+    """Handle the /hello command."""
+    # Add user to subscribers list
+    if not update.effective_user:
+        return
+
+    user_id = update.effective_user.id
+    USERS.add(user_id)
+    if update.message:
+        await update.message.reply_text(f'Hello, {user_id}! I am your bot. How can I assist you today?')
+
+
+@admin_check
 async def handle_message(update, context):
     """Handle user messages and respond them."""
     if not update.message or not update.message.from_user:
@@ -43,6 +69,7 @@ async def handle_message(update, context):
         await update.message.reply_text('something wrong')
 
 
+@admin_check
 async def echo_handler(update, context):
     """Echo the received message."""
     if update.message:
@@ -54,12 +81,15 @@ def main():
         telegram_bot(
             logger='bot-core',
             mode='pull',
+            commands=[
+                BotCommand('hello', 'Hello from bot'),
+            ],
             handlers=[
-                # Проверяем, что пользователь admin
-                TypeHandler(Update, is_admin), # type: ignore
+                # Command handlers
+                CommandHandler('hello', hello_handler),  # /hello
 
                 # Отвечаем на ответы и на призывы
-                MessageHandler(filters.TEXT & (filters.Entity('mention') | filters.REPLY), handle_message),
+                MessageHandler(filters.TEXT & ~filters.COMMAND & (filters.Entity('mention') | filters.REPLY), handle_message),
                 # Отвечаем на все, кроме команд
                 MessageHandler(filters.TEXT & ~filters.COMMAND, echo_handler),
             ],
